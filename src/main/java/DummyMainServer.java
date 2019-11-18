87594aca55412df740be5045605b9c7a1a5e0181
g521885 <Kush.GUPTA@idemia.com>
2019-11-18 15:32:45 +0530

    samle grpc added

---
+++ src/main/java/DummyMainServer.java
@@ -0,0 +1,19 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
+public class DummyMainServer {
+
+  public static void main(String[] args) {
+    System.out.println("Hello gRPC!!");
+    Server server1 = ServerBuilder.forPort(50051).build();
+    try {
+      server1.start();
+      server1.shutdownNow();
+      System.out.println("shutdown");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
